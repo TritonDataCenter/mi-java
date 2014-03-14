386761125a2242ecf1e4251b6e717e7f9eb6c361
Derek Crudgington <derek@joyent.com>
2014-03-14 13:54:07 +0000

    Added release 13.3.1

--- copy/var/zoneinit/includes/31-java.sh
+++ copy/var/zoneinit/includes/31-java.sh
@@ -1,5 +1,6 @@
-# Get password from metadata, unless passed as TOMCAT_PW
-TOMCAT_PW=${TOMCAT_PW:-$(mdata-get tomcat_pw 2>/dev/null)};
+# Get password from metadata unless passed as TOMCAT_PW, or set one
+TOMCAT_PW=${TOMCAT_PW:-$(mdata-get tomcat_pw 2>/dev/null)} || \
+TOMCAT_PW=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ');
 
 # Remove last line from tomcat-users.xml
 sed -i '$ d' /opt/local/share/tomcat/conf/tomcat-users.xml
